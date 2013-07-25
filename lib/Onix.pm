b08a8d453502dc5f0de49b662a49db2b42e6b74f
Nitish Bezzala <nbezzala@yahoo.com>
2013-07-25 10:09:28 -0500

    Inserting Product Identifier value along with the Product.

--- lib/Onix.pm
+++ lib/Onix.pm
@@ -1,5 +1,11 @@
 package Onix;
 
+=head2
+The lib file which is called from the .pl which is run by the user.
+Used to read the file name from the command line
+And to creat the product
+=cut
+
 use Moose;
 with 'Onix::DB';
 with 'MooseX::Getopt::Strict';
@@ -10,7 +16,7 @@ has 'file' => (
 			  );
 
 =head2
-Todo: add  transactions.
+Todo: add  transactions if needed.
 =cut
 sub create_product {
 	my $self 	= shift;
@@ -18,7 +24,12 @@ sub create_product {
 
 	my $new_record = $self->schema->resultset('Product')->create({
 		reference	=> $product->record_ref,
+		identifier	=> {
+			idtypecode	=> $product->prod_ident->pid_type,
+		}	
 	});
 }
+#print $product->prod_ident->pid_type, "\n";
+#print $product->prod_ident->id_value, "\n";
 
 1;
