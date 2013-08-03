5775c5ac5242280d8152b098b7a40c1c203c57b2
Nitish Bezzala <nbezzala@yahoo.com>
2013-08-03 07:33:08 -0500

    Added tables to store SupplyDetail Price and Stock.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,14 +1,13 @@
--- create_tables.sql
+-- create_tables.sql  -- for mysql only
 
 -- Each Product has 
---  one ProductIdentifier
---  one DescriptiveDetail
+--  one ProductIdentifier, one DescriptiveDetail...
 DROP TABLE IF EXISTS Product;
 CREATE TABLE Product (
 	ID INTEGER primary key auto_increment,
 	Reference VARCHAR(100) unique NOT NULL, -- RecordReference
-	IdentifierID integer references ProductIdentifier(ID),
-	DescDetailID integer references DescriptiveDetail(ID)
+	IdentifierID integer NOT NULL references ProductIdentifier(ID),
+	DescDetailID integer NOT NULL references DescriptiveDetail(ID)
 );
 
 DROP TABLE IF EXISTS ProductIdentifier;
@@ -23,7 +22,7 @@ create table ProductIdentifier (
 drop table if exists FormDetail;
 create table FormDetail (
 	ID integer primary key auto_increment,
-	DescriptiveDetailID integer references DescriptiveDetail(ID),
+	DescriptiveDetailID integer NOT NULL references DescriptiveDetail(ID),
 	FormDetail char(4) -- a letter followed by 3 digits
 );
 
@@ -32,7 +31,7 @@ create table FormDetail (
 drop table if exists ProductFormFeature;
 create table ProductFormFeature (
 	ID integer primary key auto_increment,
-	DescriptiveDetailID integer references ProductForm(ID),
+	DescriptiveDetailID integer NOT NULL references ProductForm(ID),
 	FeatureType 	int(2),
 	FeatureValue 	char(2), -- code used depends on FeatureType, add check functions in the model
 	FeatureDesc 	text(500)
@@ -47,6 +46,7 @@ create table DescriptiveDetail (
 	FormCode 	char(2) not null
 );
 
+-- May use this to validate the Codes from the .csv file of code list values
 drop table if exists CodeList;
 CREATE TABLE CodeList (
 	Number INT(2),
@@ -58,26 +58,20 @@ CREATE TABLE CodeList (
 )  ;
 
 
-drop table if exists SupplierIdentifier;
-create table SupplierIdentifier (
-	ID integer primary key auto_increment
-	
-);
-
 -- Product has a ProductSupply for each market (and there can be many markets)
 -- Product has many ProductSupply , ProductSupply belongs to a Product
 -- Required, unless NotificationType is Update
 drop table if exists ProductSupply;
 create table ProductSupply (
 	ID integer primary key auto_increment,
-	productID integer references Product(ID)
+	productID integer NOT NULL references Product(ID)
 );
 
 -- a ProductSupply has many SupplyDetails
 drop table if exists SupplyDetail;
 create table SupplyDetail (
 	ID integer primary key auto_increment,
-	ProductSupplyID integer references ProductSupply(ID),
+	ProductSupplyID integer NOT NULL references ProductSupply(ID),
 	SupplierID integer references Supplier(ID),
 	ProductAvailability int(2) NOT NULL -- code list 65	
 );
@@ -96,7 +90,7 @@ create table Supplier (
 drop table if exists SupplierIdentifier;
 create table SupplierIdentifier (
 	ID integer primary key auto_increment,
-	SupplierID integer references Supplier(ID),
+	SupplierID integer NOT NULL references Supplier(ID),
 	TypeCode int(2), -- code list 92
 	TypeName varchar(50),  -- Read doc for this later
 	IDValue	varchar(255) -- depends on the TypeCode	
@@ -105,8 +99,42 @@ create table SupplierIdentifier (
 drop table  if exists OnOrderDetail;
 create table OnOrderDetail (
 	ID integer primary key auto_increment,
-	SupplyDetailID integer references SupplyDetail(ID),
+	SupplyDetailID integer NOT NULL references SupplyDetail(ID),
 	ShipmentExpectedDate date -- format YYYYMMDD, unless format is specified
 );
 
+-- SupplyDetail can have more than one SupplyDate
+-- For now, store the date and the date format (or just assume default format)
+-- To Do: Convert the String into a DateTime and store it.
+drop table if exists SupplyDate;
+create table SupplyDate (
+	ID integer primary key auto_increment,
+	SupplyDetailID integer NOT NULL references SupplyDetail(ID),
+	SupplyDateRole int(2), -- List 166, 08 is code for Expected Availability Date
+	Date varchar(12)  -- maybe DateTime or Varchar.. need to see docs and decide
+);
 
+-- Stock has many OnHand, but only one or zero OnOrder
+-- OnHand is mandatory, but can be zero
+drop table if exists Stock;
+create table Stock (
+	ID integer primary key auto_increment,
+	SupplyDetailID integer NOT NULL references SupplyDetail(ID),
+	OnOrder int(7)
+);
+
+drop table if exists OnHand;
+create table OnHand (
+	ID integer primary key auto_increment,
+	StockID integer NOT NULL references Stock(ID),
+	OnHand int(7)
+);
+
+drop table if exists Price;
+create table Price (
+	ID integer primary key auto_increment,
+	SupplyDetailID integer NOT NULL references SupplyDetail(ID),
+	PriceType int(2), -- code list 58	
+	PriceAmount decimal(16,4), -- assuming no one uses more decimal points
+	CurrencyCode int(2) -- code list 96	
+);
